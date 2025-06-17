6ceaed1ef352fb69160325a747044a037ce6ccc7
Recognizer83 <usamashamsi2013@gmail.com>
2025-06-18 01:25:34 +0600

    provision

---
+++ files/etc/wezterm/wezterm.lua
@@ -0,0 +1,14 @@
+local wezterm = require 'wezterm'
+local config = {}
+
+config.hide_tab_bar_if_only_one_tab = true
+config.window_padding = {
+	left = '24pt',
+	right = '24pt',
+	top = '18pt',
+	bottom = '18pt',
+}
+config.use_fancy_tab_bar = false
+
+
+return config
